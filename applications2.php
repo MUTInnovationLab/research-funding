7dd29df5dff797a5fc2f4a0f290407ab9bde3e36
vusumuzi <vgwala149@gmail.com>
2024-03-19 11:51:13 +0200

    j

--- applications2.php
+++ applications2.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="style.css" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css" />
     <title>Applications</title>
     <style>
         body {
@@ -91,10 +90,6 @@
             height:100% !important;
 
         }
-      .table-container{
-          max-height: 450px;
-        }
-       
     </style>
 </head>
 <?php
